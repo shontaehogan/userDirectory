cb4eab92388935f189b9ea7bbf6235bf44d2c136
Shontae Hogan <shontaehogan@gmail.com>
2017-08-04 11:34:05 -0400

    made changes to main.js to implement switch to individual profiles

--- main.js
+++ main.js
@@ -9,34 +9,28 @@ const data = require('./data.js');
 //this sets up mustache to run as an engine for our code
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', './views')
+app.set('views', './views');
 
 // this tells the app to use the files in the PUBLIC folder
 app.use(express.static('public'));
 
-//this sends my INDEX.HTML page to the browser
-app.get('/', function(req, res) {
-    res.render('index.mustache', {users:data.users});
+
+// render index page and get variable
+app.get('/', function(req, res){
+  res.render('index', data);
+  console.log(data);
+})
+
+// gets and render robot to userInfo.mustache page
+app.get('/user/:id', function(req, res){
+  let robot = data.users.find(function(item){
+    return item.id == req.params.id;
+  });
+  res.render('userInfo', robot);
 });
 
-app.get('/:indexNum/', function (req, res) {
-
-// create variable for index
-let i = parseInt(req.params.indexNum) - 1;
-// let user = data.users.find(function(user) {return user.username == req.params.username});
-// for (let i = 0; i < data.users.length; i++) {
-
-res.render('user.mustache', data. users[i]);
-
-});
-//
-//
-//     break;
-//   }
-// }
-// // res.render('robotItem', robotItem);
 
 //this tells the browser which PORT to use
 app.listen(3000, function () {
   console.log('Successful!')
-})
+});
